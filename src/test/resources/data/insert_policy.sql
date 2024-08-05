7be263553713bd03d1e5c57854f1d06595810cf2
alizaanos <fireandaiz@gmail.com>
2024-08-05 22:11:06 +0800

    Add integration test

---
+++ src/test/resources/data/insert_policy.sql
@@ -0,0 +1,5 @@
+INSERT INTO policy
+(policy_id, policy_number, owner_first_name, owner_last_name, effectivity_date, date_created,
+last_update)
+VALUES (2, 'POL24352345', 'Jane', 'Doe', '2022-01-03', '2024-08-01 10:00:00', '2024-08-01 10:00:00')
+;
